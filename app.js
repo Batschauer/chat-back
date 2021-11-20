7c78daebeeaeff15cbfa0b611092aed69f2021ac
batschauer <nathan01batschauer@gmail.com>
2021-11-19 21:14:50 -0300

    Singup working.

--- app.js
+++ app.js
@@ -1,31 +1,30 @@
 const express = require('express');
-const { open, readFile, writeFile } = require('fs');
+const { readFile, writeFile } = require('fs');
 const app = express();
 const port = 3031;
 
-app.post('/singup', async(req, res) => {
+app.get('/singup', async(req, res) => {
     res.header('Access-Control-Allow-Headers', '*');
     res.header('Access-Control-Allow-Origin', '*');
 
     const { userName, password } = req.query;
-    console.log(req.query);
-    const storageUsers = await readFile('./data/users.json', function(e) {
-        console.log('Error: ', e);
-    });
-    let _users = storageUsers ? storageUsers.users : [];
+    console.log('Query: ', req.query);
+    
+    const storageUsers = await require('./data/users.json');
+    let _users = storageUsers ? storageUsers['users'] : [];
 
     let newUser = {
         id: _users.length + 1,
-        userName,
-        password,
+        userName: userName,
+        password: password,
         publicKey: '@TODO',
     };
 
     _users.push(newUser);
 
     const json = JSON.stringify({ users: _users });
-    await writeFile('./data/users.json', json, 'utf-8', function(e) {
-        console.log('Error: ', e);
+    await writeFile('./data/users.json', json, { flag: 'w+' }, function(e) {
+        //console.log('Error: ', e);
     });
 
     res.send(`private key`);
